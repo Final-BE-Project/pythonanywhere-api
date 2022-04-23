1be6033528f28dd337836e91cd481490bb972a6b
anirudhbelwadi <anirudh.belwadi@gmail.com>
2022-04-23 16:54:48 +0530

    api final

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, render_template, request
 import ast
 app = Flask(__name__)
 
@@ -16,13 +16,18 @@ def testfn():
     if request.method == 'POST':
         data = request.get_data()
         data = ast.literal_eval(data.decode('utf-8'))
-
-        with open("index.html", "w") as file:
-            file.write(str(data))
-        message = {'greeting': 'Hello from Flask!'}
+        print(data)
+        # with open("index.html", "w") as file:
+        # file.write(str(data))
+        message = {'message': data+' from Flask!'}
         response = jsonify(message)
         response.headers.add('Access-Control-Allow-Origin', '*')
         return response
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 
 app.run(debug=True)
